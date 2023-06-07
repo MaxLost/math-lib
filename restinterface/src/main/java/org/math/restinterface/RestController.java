1ba3de8cfe0d997b1bc4d71e650696206d92abb5
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-06-07 18:51:55 +0300

    Fixed parsing of JSON

--- restinterface/src/main/java/org/math/restinterface/RestController.java
+++ restinterface/src/main/java/org/math/restinterface/RestController.java
@@ -1,25 +1,46 @@
 package org.math.restinterface;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.math.computations.PlanePoint;
 import org.math.computations.functions.AlgebraicInterpolator;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/rest")
 public class RestController {
 
     @PostMapping("/interpolate/value")
-    public RequestResult interpolateGetValue(@RequestParam List<PlanePoint> table, @RequestParam Double point) {
+    public RequestResult interpolateGetValue(@RequestBody String requestBody) {
 
-        AlgebraicInterpolator interpolator = new AlgebraicInterpolator(table);
-        double result = interpolator.interpolate(Math.max(table.size(), 8), point);
+        try {
 
-        return new RequestResult("Success", Double.toString(result));
+            ObjectMapper mapper = new ObjectMapper();
 
+            JsonNode body = mapper.readTree(requestBody);
+            Map<String, Object> table = mapper.convertValue(body.get("table"), new TypeReference<Map<String, Object>>(){});
+
+            List<PlanePoint> nodes = new ArrayList<>();
+            for (Map.Entry<String, Object> row : table.entrySet()) {
+                nodes.add(new PlanePoint(Double.parseDouble(row.getKey()), Double.parseDouble((String) row.getValue())));
+            }
+            double x = Double.parseDouble(body.get("point").textValue());
+
+            AlgebraicInterpolator interpolator = new AlgebraicInterpolator(nodes);
+            double result = interpolator.interpolate(Math.min(table.size() - 1, 8), x);
+
+            return new RequestResult("Success", Double.toString(result));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new RequestResult("Failed", "Request processing failed");
+        }
     }
 
 }
